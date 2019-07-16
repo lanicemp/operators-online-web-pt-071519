9ffdb8c925253416e3cd3786c542a2545e1105d5
LaNice Powell <lanicemp@aol.com>
2019-07-16 16:56:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,8 @@ speed > 60 ? true : false || speed < 40 ? true : false
 end
 
 def not_safe?(speed)
-  speed > 60 ? true : false && speed < 40 ? true : false 
+  (speed > 60 ? true : false) && (speed < 40 ? true : false)
+  
 	
 end
 	
